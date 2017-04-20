779f909d6fffc83bcac3c6e6aef3071481a275d1
= <2237075053@qq.com>
2017-04-20 12:25:12 +0800

    sort

--- beautiful/src/beautiful/Sort.java
+++ beautiful/src/beautiful/Sort.java
@@ -25,4 +25,27 @@ public class Sort {
 		a[low] = key;
 		return low;
 	}
+	public void MergeSort(int [] a,int low,int high){
+		if(null==a||a.length<2) return ;
+		if(low<high){
+			int mid=(low+high)/2;
+			MergeSort(a, low, mid);
+			MergeSort(a, mid+1, high);
+			Merge(a,low,mid,high);
+		}
+	}
+	public void Merge(int [] a ,int low,int mid,int high){
+		int [] temp = new int[high-low+1];
+		int i = low;
+		int j = mid+1;
+		int k = 0;
+		while(i<=mid&&j<high){
+			if(a[i]<= a[j])  temp[k++] = a[i++];
+			else temp[k++] = a[j++];
+		}
+		while(i<=mid) temp[k++] = a[i++];
+		while(j<=high) temp[k++] = a[j++];
+		for(int t=0;t<temp.length;t++)
+			temp[low+t] = a[t];
+	}
 }
